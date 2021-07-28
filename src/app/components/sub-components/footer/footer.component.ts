49c5fbb7e734e52312131a8c659e58583da51c80
Natalia Guerrero <nathaliiaa_best@hotmail.com>
2021-07-28 02:52:38 +0200

    Footer e intento de arreglo

--- src/app/components/sub-components/footer/footer.component.ts
+++ src/app/components/sub-components/footer/footer.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+isMobile: boolean = false;
+getIsMobile(): boolean {
+  const w = document.documentElement.clientWidth;
+  const breakpoint = 992;
+  if (w < breakpoint) {
+    return true;
+  } else {
+    return false;
+  }
+}
   constructor() { }
 
   ngOnInit(): void {
+    this.isMobile = this.getIsMobile();
   }
-
+  
 }
